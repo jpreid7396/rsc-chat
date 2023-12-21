787d3e965adede9567a7ff1e594b8b18fada12a3
Julia Reid <julia.reid22@gmail.com>
2023-12-21 15:20:27 -0500

    updated firebase config

--- src/lib/firebase-client.ts
+++ src/lib/firebase-client.ts
@@ -3,14 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 export const app = initializeApp({
-  apiKey: "AIzaSyAcSi9nJy77TXVHoiZfhERzez_NlbK3F90",
-  authDomain: "nextjs-firestore-73e34.web.app",
-  databaseURL: "https://nextjs-demo-73e34-default-rtdb.firebaseio.com",
-  projectId: "nextjs-demo-73e34",
-  storageBucket: "nextjs-demo-73e34.appspot.com",
-  messagingSenderId: "1031831013463",
-  appId: "1:1031831013463:web:2a3e8f715c262020f63b12",
-  measurementId: "G-SNWQHVDPKK",
+  apiKey: "AIzaSyD3xPI4TkU7SkIg0w1iuTZ5Dnk9rj6GC5k",
+  authDomain: "julia-firestack-bugbash.firebaseapp.com",
+  projectId: "julia-firestack-bugbash",
+  storageBucket: "julia-firestack-bugbash.appspot.com",
+  messagingSenderId: "407295725690",
+  appId: "1:407295725690:web:931de7133dd1dc9d972bc3"
 });
 
 export const firestore = getFirestore(app);
