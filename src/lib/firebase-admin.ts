2aa70791a25693b73a1b5f37a36f6d5f6b748ba1
Julia Reid <julia.reid22@gmail.com>
2023-12-28 19:40:48 -0500

    try again

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,11 +1,11 @@
-import { initializeApp as initializeAdmin, cert, getApps, getApp } from 'firebase-admin/app';
+import { cert, initializeApp as initializeAdmin, getApps, getApp } from 'firebase-admin/app';
 import { getFirestore as getAdminFirestore } from 'firebase-admin/firestore';
 
 export const app = getApps().length ? getApp() : initializeAdmin({
     credential: cert({
-        client_email: process.env.FIREBASE_CLIENT_EMAIL,
-        private_key: process.env.FIREBASE_PRIVATE_KEY,
-        project_id: 'julia-firestack-bugbash'
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        projectId: 'julia-firestack-bugbash'
     })
 });
 
