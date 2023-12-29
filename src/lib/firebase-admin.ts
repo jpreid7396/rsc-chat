a7facc3a71b4dcb659d6a2049a77d533e2fa5b4d
Julia Reid <julia.reid22@gmail.com>
2023-12-28 19:45:02 -0500

    parsing private key

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -4,7 +4,7 @@ import { getFirestore as getAdminFirestore } from 'firebase-admin/firestore';
 export const app = getApps().length ? getApp() : initializeAdmin({
     credential: cert({
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
         projectId: 'julia-firestack-bugbash'
     })
 });
