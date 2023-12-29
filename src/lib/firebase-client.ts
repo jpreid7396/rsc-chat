db1aded7df707cd919e60208eb8f71a1a1e57dae
Julia Reid <julia.reid22@gmail.com>
2023-12-28 19:02:30 -0500

    trying something else

--- src/lib/firebase-client.ts
+++ src/lib/firebase-client.ts
@@ -5,7 +5,7 @@ import { getFirestore } from "firebase/firestore";
 export const app = initializeApp({
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "julia-firestack-bugbash.firebaseapp.com",
-  projectId: "julia-firestack-bugbash",
+  projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: "julia-firestack-bugbash.appspot.com",
   messagingSenderId: "407295725690",
   appId: "1:407295725690:web:931de7133dd1dc9d972bc3"
