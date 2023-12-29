c9277d9a1fca0c989f2f3ac3d2c90f966d630631
Julia Reid <julia.reid22@gmail.com>
2023-12-28 19:25:05 -0500

    added cert to initialize admin

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,7 +1,14 @@
-import { initializeApp as initializeAdmin, getApps, getApp } from 'firebase-admin/app';
+import { initializeApp as initializeAdmin, cert, getApps, getApp } from 'firebase-admin/app';
 import { getFirestore as getAdminFirestore } from 'firebase-admin/firestore';
 
-export const app = getApps().length ? getApp() : initializeAdmin();
+export const app = getApps().length ? getApp() : initializeAdmin({
+    credential: admin.credential.cert({
+        client_email: process.env.FIREBASE_CLIENT_EMAIL,
+        private_key: process.env.FIREBASE_PRIVATE_KEY,
+        project_id: 'julia-firestack-bugbash'
+    })
+});
+
 export const firestore = getAdminFirestore(app);
 
 export const messageSnapshots = firestore
--- src/lib/firebase-client.ts
+++ src/lib/firebase-client.ts
@@ -8,8 +8,7 @@ export const app = initializeApp({
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: "julia-firestack-bugbash.appspot.com",
   messagingSenderId: "407295725690",
-  appId: "1:407295725690:web:931de7133dd1dc9d972bc3",
-  credential: applicationDefault()
+  appId: "1:407295725690:web:931de7133dd1dc9d972bc3"
 });
 
 export const firestore = getFirestore(app);
